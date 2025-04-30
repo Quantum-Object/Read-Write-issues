9f4b76e4c91f0bc09d7a00820bed230e81ab0ba1
Amr Sorour <164084716+amoorgy@users.noreply.github.com>
2025-04-30 23:33:48 +0300

    Update write_priority.c
    fixed deadlock caused by writer thread

--- write_priority.c
+++ write_priority.c
@@ -32,27 +32,25 @@ void init_data() {
 void* reader(void* arg) {
     int id = *(int*)arg;
     while (1) {
-        // Block if there are writers (active or waiting)
-        sem_wait(&data.read_sem);
-        
         sem_wait(&data.mutex);
-        data.readers++;
-        if (data.readers == 1) {
-            sem_wait(&data.write_sem); // First reader locks writers out
+        if (data.waiting_writers > 0 || data.writers > 0) {
+            sem_post(&data.mutex);
+            continue; // retry later
         }
+        data.readers++;
+        if (data.readers == 1)
+            sem_wait(&data.write_sem); // first reader locks writers
         sem_post(&data.mutex);
-        
-        sem_post(&data.read_sem); // Allow other readers to proceed
 
         printf("Reader %d reads: %d\n", id, data.value);
         sleep(1);
 
         sem_wait(&data.mutex);
         data.readers--;
-        if (data.readers == 0) {
-            sem_post(&data.write_sem); // Last reader releases write lock
-        }
+        if (data.readers == 0)
+            sem_post(&data.write_sem); // last reader releases writers
         sem_post(&data.mutex);
+        sleep(1);
     }
     return NULL;
 }
@@ -62,24 +60,21 @@ void* writer(void* arg) {
     while (1) {
         sem_wait(&data.mutex);
         data.waiting_writers++;
-        if (data.waiting_writers == 1) {
-            sem_wait(&data.read_sem); // First waiting writer blocks new readers
-        }
         sem_post(&data.mutex);
 
-        sem_wait(&data.write_sem); // Get exclusive access
-        data.writers++;
+        sem_wait(&data.write_sem); // exclusive access
+
+        sem_wait(&data.mutex);
         data.waiting_writers--;
-        
+        data.writers++;
+        sem_post(&data.mutex);
+
         data.value++;
         printf("Writer %d writes: %d\n", id, data.value);
         sleep(2);
 
         sem_wait(&data.mutex);
         data.writers--;
-        if (data.waiting_writers == 0) {
-            sem_post(&data.read_sem); // Unblock readers if no more writers
-        }
         sem_post(&data.mutex);
 
         sem_post(&data.write_sem);
@@ -94,16 +89,17 @@ int main() {
     
     init_data();
 
-    for (int i = 0; i < NUM_READERS; i++) {
-        reader_ids[i] = i + 1;
-        pthread_create(&readers[i], NULL, reader, &reader_ids[i]);
-    }
+
     for (int i = 0; i < NUM_WRITERS; i++) {
         writer_ids[i] = i + 1;
         pthread_create(&writers[i], NULL, writer, &writer_ids[i]);
     }
+    for (int i = 0; i < NUM_READERS; i++) {
+        reader_ids[i] = i + 1;
+        pthread_create(&readers[i], NULL, reader, &reader_ids[i]);
+    }
     
-    sleep(10);
+    sleep(30);
     
     sem_destroy(&data.mutex);
     sem_destroy(&data.write_sem);
